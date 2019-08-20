bf2a4d9d331fa0c73039ffcc59f710c04e35e09c
Florent Biville <fbiville@pivotal.io>
2019-08-20 18:07:34 +0200

    Fix deploy script

--- ci/deploy-image.sh
+++ ci/deploy-image.sh
@@ -2,16 +2,20 @@
 
 set -Eeuxo pipefail
 
+function get_maven_project_version() {
+  mvn -q org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -DforceStdout
+}
+
 function deploy() {
   mvn -B com.google.cloud.tools:jib-maven-plugin:1.3.0:build \
     -Djib.to.image="${1}" \
     -Djib.to.auth.username="${DOCKER_USER}" \
-    -Djib.to.auth.username="${DOCKER_PASS}"
+    -Djib.to.auth.password="${DOCKER_PASS}"
 }
 
 function main() {
   base_image="projectriff/streaming-processor"
-  version=$(mvn -q org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version)
+  version=$(get_maven_project_version)
 
   echo "Deploying ${base_image} (latest and ${version})"
   deploy "${base_image}"
