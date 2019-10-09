40fc2cbb3d8e3ad7017830f72e03bcbd755397c9
Scott Andrews <sandrews@pivotal.io>
2019-10-09 18:29:59 -0400

    Switch Travis to GitHub Actions (#12)
    - use maven wrapper
    - add commit sha as an image tag

---
+++ .github/workflows/deploy-image.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -Eeuxo pipefail
+
+function get_maven_project_version() {
+  ./mvnw -q org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -DforceStdout
+}
+
+function deploy() {
+  ./mvnw -B com.google.cloud.tools:jib-maven-plugin:1.3.0:build \
+    -Djib.to.image="${1}" \
+    -Djib.to.auth.username="${DOCKER_USERNAME}" \
+    -Djib.to.auth.password="$(echo ${DOCKER_PASSWORD} | base64 --decode)"
+}
+
+function main() {
+  base_image="projectriff/streaming-processor"
+  version=$(get_maven_project_version)
+  commit=$(git rev-parse HEAD)
+
+  echo "Deploying ${base_image} (latest and ${version})"
+  deploy "${base_image}"
+  deploy "${base_image}:${version}"
+  deploy "${base_image}:${version}-${commit}"
+}
+
+main
